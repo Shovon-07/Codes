2d3113d3488cf6589aefaf7d01dc3cdab596151c
Shovon-07 <aljubair.shovon.07@gmail.com>
2023-09-20 04:36:52 +0600

    c

--- SQL/SQL.sql
+++ SQL/SQL.sql
@@ -1,25 +1,39 @@
-/*___ Database ___*/ 
+/*___ Database ___*/
 create database Students;
-
-drop database Students;
-
+drop database world;
 
 /*___ Table ___*/
 use Students;
+
+/* Create table */
 create table Student(
-	Id int,
-    Name varchar(50),
-    Roll varchar(10),
+	Id int primary key,
+    Name varchar(50) not null,
+    Roll varchar(10) not null,
     Phone varchar(11),
-    Depertment varchar(4)
+    Depertment varchar(3) not null
 );
 
-insert into student(
-	Id, Name, Roll, Phone, Depertment
-) values(
-	1,'Shovon', '52057', '0179999', 'CMT'
-);
+/* Add Table Field */
+alter table Student add Email varchar(50);
 
-delete from Student where Id=1;
+/* Delete table */
+drop table Student;
+
+/* Insert data in table */ 
+insert into Student
+	(Id,Name,Roll,Phone,Depertment)
+    values(4,'sexy','520143','019','ent');
+
+/* Delete data */
+delete from Student where Id=4;
+
+/* Update data */
+update Student set Roll='520120' where Id=2;
 
 select * from student;
+select * from student where Id=1;
+
+/* Sorting */
+select * from student order by Name asc;
+select * from student order by Name desc;
