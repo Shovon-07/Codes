1e6d32bd65f0e5cf0f036045a30908c35e300d8b
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-08-31 01:06:45 +0600

    Commited some changes

--- C-program/main.c
+++ C-program/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 //=>>> Function declaration
 //=>>> Task 3
@@ -53,22 +54,54 @@ int main()
     // printf("Cm to km = %f\n", kMeter);
 
     //=>>> Task 8
-    float celsius = 40;
-    float fahrenheit = (celsius * 9 / 5) + 32;
+    // float celsius = 40;
+    // float fahrenheit = (celsius * 9 / 5) + 32;
     // printf("Celsius to fahrenheit = %f\n", fahrenheit);
 
     //=>>> Task 9
     //=>>> Task 10
-    int years, weeks, days;
-    int totalDays;
-    printf("Enter days = ");
-    scanf("%d", &totalDays);
+    // int years, weeks, days;
+    // int totalDays;
+    // printf("Enter days = ");
+    // scanf("%d", &totalDays);
 
-    years = totalDays / 365;
-    weeks = (totalDays % 365) / 7;
-    days = (totalDays % 365) % 7;
+    // years = totalDays / 365;
+    // weeks = (totalDays % 365) / 7;
+    // days = (totalDays % 365) % 7;
+    // printf("Year/s = %d\nWeeks = %d\nDays = %d\n", years, weeks, days);
 
-    printf("Year/s = %d\nWeeks = %d\nDays = %d\n", years, weeks, days);
+    //=>>> Task 11 & 12
+    // int power = pow(2, 2);
+    // int squreRoot = sqrt(49);
+    // printf("Power = %d\n", power);
+    // printf("Squre root = %d\n", squreRoot);
+
+    //=>>> Task 13
+    // int angle_1 = 90, angle_2 = 45, angle_3;
+    // angle_3 = 180 - (angle_1 + angle_2);
+    // printf("Third angle = %d\n", angle_3);
+
+    //=>>> Task 14
+    // float base = 100, height = 50, area;
+    // area = ((float)1 / (float)2) * (base * height);
+    // printf("Area of triangle = %.2f\n", area);
+
+    //=>>> Task 15
+    // float area = (sqrt(3) * pow(50, 2)) / 4; // side = 50
+    // printf("Area of equilateral = %.2f\n", area);
+
+    // Task 16
+    float math = 90, phy = 86, che = 80, eee = 91, eng = 90, total, average, percentage;
+    total = math + phy + che + eee + eng;
+    average = total / (float)5;
+    percentage = (total / (float)500) * (float)100;
+    printf("Total = %.2f\n", total);
+    printf("Total = %.2f\n", average);
+    printf("Percentage = %.2f%%\n", percentage); // percentage = ((math+phy+che+eee+eng) / The sum of the highest marks in each subject) * 100
+
+    // Task 17
+    // Task 18
+    float p, t, t, compoundInterest;
 
     return 0;
 }
