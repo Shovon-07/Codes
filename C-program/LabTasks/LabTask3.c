915bedadee5ca288d1fefaa634b4c96abb29d377
Shovon-07 <aljubair.shovon.07@gmail.com>
2026-02-20 14:53:03 +0600

    Commited some changes

--- C-program/LabTasks/LabTask3.c
+++ C-program/LabTasks/LabTask3.c
@@ -114,5 +114,19 @@ void main()
     }
     */
 
-        // for()
+    //=>>> Task 13
+    int multiple;
+
+    for (int x = 1; x <= 10; x += 1)
+    {
+        for (int y = 1; y <= 10; y += 1)
+        {
+            multiple = x * y;
+            printf("%d * %d = %d\n", x, y, multiple);
+            if (y == 10)
+            {
+                printf("\n--------------\n");
+            }
+        }
+    }
 }
