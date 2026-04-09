71b4f7f58a81148963c4f8c4d976ddd909f9f50d
Shovon-07 <aljubair.shovon.07@gmail.com>
2026-04-09 14:24:13 +0600

    Commited some changes

---
+++ C-program/LabTasks/Pattern_exam.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void main()
+{
+    /**
+     * Pattern
+     * 1
+     * 0 1
+     * 1 0 1
+     * 0 1 0 1
+     * 1 0 1 0 1
+     **/
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (i % 2 == 0)
+            {
+                if (j % 2 == 0)
+                {
+                    printf("1");
+                }
+                else
+                {
+                    printf("0");
+                }
+            }
+            else
+            {
+                if (j % 2 == 0)
+                {
+                    printf("0");
+                }
+                else
+                {
+                    printf("1");
+                }
+            }
+        }
+        printf("\n");
+    }
+}
--- C-program/Pattern.c
+++ C-program/Pattern.c
@@ -257,37 +257,37 @@ void main()
      * 1 0 1
      * 0 1 0 1
      **/
-    int n;
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            if (i % 2 == 0)
-            {
-                if (j % 2 == 0)
-                {
-                    printf("1");
-                }
-                else
-                {
-                    printf("0");
-                }
-            }
-            else
-            {
-                if (j % 2 == 0)
-                {
-                    printf("0");
-                }
-                else
-                {
-                    printf("1");
-                }
-            }
-        }
-        printf("\n");
-    }
+    // int n;
+    // scanf("%d", &n);
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         if (i % 2 == 0)
+    //         {
+    //             if (j % 2 == 0)
+    //             {
+    //                 printf("1");
+    //             }
+    //             else
+    //             {
+    //                 printf("0");
+    //             }
+    //         }
+    //         else
+    //         {
+    //             if (j % 2 == 0)
+    //             {
+    //                 printf("0");
+    //             }
+    //             else
+    //             {
+    //                 printf("1");
+    //             }
+    //         }
+    //     }
+    //     printf("\n");
+    // }
 
     //=>>> Calculate execution time
     clock_t ending = clock();
