687510a4fe3df23cc98bef3d7939574fe357a639
Shovon-07 <aljubair.shovon.07@gmail.com>
2026-06-06 22:27:04 +0600

    Commited some changes

--- C-program/Assignments/ArrayAssignment.c
+++ C-program/Assignments/ArrayAssignment.c
@@ -4,14 +4,11 @@
 int main()
 {
     //=>>> Problem 1
-    // int n, sum = 0;
-    // float average;
-
-    // printf("Enter the number of students : ");
+    // int n, total = 0, avg = 0;
+    // printf("Enter the number of element : ");
     // scanf("%d", &n);
     // int marks[n];
 
-    // printf("Enter marks : ");
     // for (int i = 0; i < n; i++)
     // {
     //     scanf("%d", &marks[i]);
@@ -21,12 +18,19 @@ int main()
 
     // for (int i = 0; i < n; i++)
     // {
-    //     sum += marks[i];
-    //     highest = fmax(highest, marks[i]);
-    //     lowest = fmin(lowest, marks[i]);
+    //     total += marks[i];
+    //     if (marks[i] > highest)
+    //     {
+    //         highest = marks[i];
+    //     }
+    //     if (marks[i] < lowest)
+    //     {
+    //         lowest = marks[i];
+    //     }
     // }
-    // average = sum / n;
-    // printf("Agerage = %.2f\n", average);
+
+    // avg = total / n;
+    // printf("Avg = %d\n", avg);
     // printf("Highest = %d\n", highest);
     // printf("Lowest = %d\n", lowest);
 
@@ -34,9 +38,8 @@ int main()
     // int n, even = 0, odd = 0;
     // printf("Enter the number of element : ");
     // scanf("%d", &n);
-
     // int numbers[n];
-    // printf("Enter the numbers : ");
+
     // for (int i = 0; i < n; i++)
     // {
     //     scanf("%d", &numbers[i]);
@@ -53,43 +56,194 @@ int main()
     //         odd++;
     //     }
     // }
-    // printf("Even : %d\n", even);
-    // printf("Odd : %d\n", odd);
+    // printf("Even = %d\n", even);
+    // printf("Odd = %d\n", odd);
 
     //=>>> Problem 3
     // int n;
     // printf("Enter the number of element : ");
     // scanf("%d", &n);
+    // int arr[n];
 
-    // int numbers[n];
-    // printf("Enter values : \n");
     // for (int i = 0; i < n; i++)
     // {
-    //     scanf("%d", &numbers[i]);
+    //     scanf("%d", &arr[i]);
     // }
 
     // for (int i = n - 1; i >= 0; i--)
     // {
-    //     printf("%d ", numbers[i]);
+    //     printf("%d ", arr[i]);
     // }
 
     //=>>> Problem 4
-    int n;
-    printf("Enter the number of element : ");
-    scanf("%d", &n);
+    // int n;
+    // printf("Enter the number of element : ");
+    // scanf("%d", &n);
+    // int score[n];
 
-    int numbers[n], secondMax = numbers[0];
-    printf("Enter values : \n");
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &numbers[i]);
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     scanf("%d", &score[i]);
+    // }
 
-    for (int i = 0; i < n; i++)
-    {
-        secondMax = fmax(secondMax, numbers[i]);
-    }
-    printf("Second largest = %d\n", secondMax);
+    // int fLarge = score[0], sLarge = -1000000000;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (fLarge < score[i])
+    //     {
+    //         fLarge = score[i];
+    //     }
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (fLarge > score[i] && sLarge < score[i])
+    //     {
+    //         sLarge = score[i];
+    //     }
+    // }
+    // printf("First largest = %d\n", fLarge);
+    // printf("Second largest = %d\n", sLarge);
+
+    //=>>> Problem 5
+    // int n, maxValue = 0;
+    // printf("Enter the number of element : ");
+    // scanf("%d", &n);
+    // int numbers[n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     scanf("%d", &numbers[i]);
+    //     if (numbers[i] > maxValue)
+    //     {
+    //         maxValue = numbers[i];
+    //     }
+    // }
+
+    // int freqArr[maxValue + 1], range = maxValue + 1;
+
+    // for (int i = 0; i < range; i++)
+    // {
+    //     freqArr[i] = 0;
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int x = numbers[i];
+    //     freqArr[x]++;
+    // }
+
+    // for (int i = 0; i < range; i++)
+    // {
+    //     if (freqArr[i] != 0)
+    //     {
+    //         printf("%d occurs %d times\n", i, freqArr[i]);
+    //     }
+    // }
+
+    //=>>> Problem 6
+    // int x, y, i, j;
+    // printf("Enter the row size : ");
+    // scanf("%d", &x);
+    // printf("Enter the column size : ");
+    // scanf("%d", &y);
+
+    // int a[x][y], b[x][y], result[x][y];
+
+    // printf("Enter the element of first matrix : \n");
+    // for (i = 0; i < x; i++)
+    // {
+    //     for (j = 0; j < y; j++)
+    //     {
+    //         scanf("%d", &a[i][j]);
+    //     }
+    // }
+    // printf("Enter the element of second matrix : \n");
+    // for (i = 0; i < x; i++)
+    // {
+    //     for (j = 0; j < y; j++)
+    //     {
+    //         scanf("%d", &b[i][j]);
+    //     }
+    // }
+
+    // for (i = 0; i < x; i++)
+    // {
+    //     for (j = 0; j < y; j++)
+    //     {
+    //         result[i][j] = a[i][j] + b[i][j];
+    //     }
+    // }
+    // printf("Result of matrix addition : \n");
+    // for (i = 0; i < x; i++)
+    // {
+    //     for (j = 0; j < y; j++)
+    //     {
+    //         printf("%d ", result[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    //=>>> Problem 7
+    // int row, col, i, j;
+    // printf("Enter the row size : ");
+    // scanf("%d", &row);
+    // printf("Enter the column size : ");
+    // scanf("%d", &col);
+
+    // int arr[row][col];
+
+    // printf("Enter the element of array : \n");
+    // for (i = 0; i < row; i++)
+    // {
+    //     for (j = 0; j < col; j++)
+    //     {
+    //         scanf("%d", &arr[i][j]);
+    //     }
+    // }
+    // for (i = 0; i < row; i++)
+    // {
+    //     int sum = 0;
+    //     for (j = 0; j < col; j++)
+    //     {
+    //         sum += arr[i][j];
+    //     }
+    //     printf("Row %d Sum = %d\n", i + 1, sum);
+    // }
+
+    //=>>> Problem 8
+    // int row, col, i, j;
+    // printf("Enter the row size : ");
+    // scanf("%d", &row);
+    // printf("Enter the column size : ");
+    // scanf("%d", &col);
+
+    // int arr[row][col];
+
+    // printf("Enter the element of array : \n");
+    // for (i = 0; i < row; i++)
+    // {
+    //     for (j = 0; j < col; j++)
+    //     {
+    //         scanf("%d", &arr[i][j]);
+    //     }
+    // }
+    // int arr2[col][row];
+    // for (i = 0; i < row; i++)
+    // {
+    //     for (j = 0; j < col; j++)
+    //     {
+    //         arr2[j][i] = arr[i][j];
+    //     }
+    // }
+    // for (i = 0; i < col; i++)
+    // {
+    //     for (j = 0; j < row; j++)
+    //     {
+    //         printf("%d ", arr2[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
     return 0;
 }
--- C-program/main.c
+++ C-program/main.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
 
-void func(int *x, int y)
-{
-    // printf("Value of x = %d\n", *x);
-    printf("Address of x = %p\n", x);
-    int sum = *x + y;
-    printf("Sum = %d\n", sum);
-}
-
 int main()
 {
-    int a = 3, b = 2;
-    func(&a, b);
-    // printf("Value of p = %d\n", *p);
-    // printf("Address of p = %p\n", p);
 
     return 0;
 }
