4218e7cf775ec838a162a8789250b0fe3b4c38f5
Shovon-07 <aljubair.shovon.07@gmail.com>
2026-07-16 22:16:58 +0600

    Commited some changes

---
+++ C++Project/include/patternclass.h
@@ -0,0 +1,17 @@
+#ifndef PATTERNCLASS_H
+#define PATTERNCLASS_H
+
+
+class PatternClass
+{
+public:
+    PatternClass();
+    void LeftPyramid();
+    void RightPyramid();
+
+protected:
+
+private:
+};
+
+#endif // PATTERNCLASS_H
